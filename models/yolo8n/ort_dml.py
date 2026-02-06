e137f4e7f53faf8a92eacec1e9664d8b55002b26
Krivoruchko <gkrivoru@amd.com>
2026-02-06 10:51:24 +0400

    Added DirectML Execution Provider tests

---
+++ models/yolo8n/ort_dml.py
@@ -0,0 +1,41 @@
+import os
+from class_model import Model
+import numpy as np
+import onnxruntime as ort
+
+class Model(Model):
+  def __init__(self):
+    super().__init__()
+    self.sess = None
+    self.sess_data = {'providers': ['DmlExecutionProvider']}
+    self.model_path = 'yolov8n_{batch}b.onnx'
+    self.model_description = 'YOLOv8n inference with using DirectML Execution Provider'
+    if not self.sess_data['providers'][0] in ort.get_available_providers():
+      raise Exception(f'DirectML Execution Provider is not available')
+  def prepare_batch(self, batch_size):
+    file_path = self.get_file_path(self.model_path.format(batch=batch_size))
+    if not os.path.exists(file_path):
+      try:
+        yolo_model_path = 'yolov8n.pt'
+        if os.path.exists(yolo_model_path):
+          from ultralytics import YOLO
+          model = YOLO(yolo_model_path)
+          model.export(format='onnx', imgsz=640, batch=batch_size)
+          os.rename(yolo_model_path[:-2] + 'onnx', file_path)
+        else:
+          raise Exception(f'YOLO model file {yolo_model_path} not found')
+      except Exception as e:
+        raise Exception(f'Failed to export model {e}')
+    pass
+  def read(self):
+    file_path = self.get_file_path(self.model_path.format(batch=self.batch_size))
+    self.sess = ort.InferenceSession(file_path, **self.sess_data)
+  def prepare(self):
+    self.input_data = {
+      'images': np.random.randn(self.batch_size, 3, 640, 640).astype(np.float32),
+    }
+  def inference(self):
+    #return self.sess.run(['output0'], input_feed=self.input_data)
+    return self.sess.run([], input_feed=self.input_data)
+  def shutdown(self):
+    pass
