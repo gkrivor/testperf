21e54c5e0bbbe2544ea759dd1d8ff5da787ccd66
Krivoruchko <gkrivoru@amd.com>
2026-02-06 21:09:54 +0400

    Added model loading approach

--- models/yolo11l/common.py
+++ models/yolo11l/common.py
@@ -4,6 +4,12 @@ def try_export_model(file_path, batch_size):
     if not os.path.exists(file_path):
       try:
         yolo_model_path = 'yolov11l.pt'
+        if not os.path.exists(yolo_model_path):
+          try:
+            import urllib.request
+            urllib.request.urlretrieve('https://github.com/ultralytics/assets/releases/download/v8.4.0/yolo11l.pt', yolo_model_path)
+          except Exception as e:
+            raise Exception(f'Failed to download YOLO model {e}')
         if os.path.exists(yolo_model_path):
           from ultralytics import YOLO
           model = YOLO(yolo_model_path)
--- models/yolo8n/common.py
+++ models/yolo8n/common.py
@@ -4,6 +4,12 @@ def try_export_model(file_path, batch_size):
     if not os.path.exists(file_path):
       try:
         yolo_model_path = 'yolov8n.pt'
+        if not os.path.exists(yolo_model_path):
+          try:
+            import urllib.request
+            urllib.request.urlretrieve('https://github.com/ultralytics/assets/releases/download/v8.4.0/yolov8n.pt', yolo_model_path)
+          except Exception as e:
+            raise Exception(f'Failed to download YOLO model {e}')
         if os.path.exists(yolo_model_path):
           from ultralytics import YOLO
           model = YOLO(yolo_model_path)
